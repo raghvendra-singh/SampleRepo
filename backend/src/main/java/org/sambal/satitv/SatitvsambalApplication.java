299f59ca3a911a0559257a373916c97930e15de3
Raghvendra Singh <raghvendrasingh@Raghvendras-MacBook-Air.local>
2025-05-17 21:16:46 +0530

    first commit

---
+++ backend/src/main/java/org/sambal/satitv/SatitvsambalApplication.java
@@ -0,0 +1,13 @@
+package org.sambal.satitv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatitvsambalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatitvsambalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/sambal/satitv/SatitvsambalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sambal.satitv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatitvsambalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
